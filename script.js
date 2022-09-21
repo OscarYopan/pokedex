d6d51b9b5a7b097d0084f32af2cac85311e1eef1
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-09-20 23:36:24 -0300

    --

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ const colors = {
   bug: '#f8d5a3',
   dragon: '#97b3e6',
   psychic: '#eaeda1',
-  flying: '#F5F5F5',
+  flying: '#e6bd62',
   fighting: '#E6E0D4',
-  normal: '#F5F5F5'
+  normal: '#909090'
 }
+
+const mainTypes = Object.keys(colors)
 
 const fetchPokemons = async () => {
   for (let i = 1; i <= pokeomCount; i++) {
@@ -28,25 +30,32 @@ const getPokemon = async id => {
   const res = await fetch(url)
   const data = await res.json()
   createPokemonCard(data)
+  console.log(data);
 }
 
 const createPokemonCard = pokemon => {
   const pokemonEl = document.createElement('div')
   pokemonEl.classList.add('pokemon')
 
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+  const id = pokemon.id.toString().padStart(3, '0')
+  const pokeTypes = pokemon.types.map(type => type.type.name)
+  const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1)
+  const color = colors[type]
+  pokemonEl.style.backgroundColor = color
+
   const pokemonInnerHTML = `
-  <div class="pokemon" style="background-color: #0A285F">
     <div class="img-container">
-      <!-- <img src="" alt="Exemplo"> -->
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${name}">
     </div>
     <div class="info">
-      <span class="number">#001</span>
-      <h3 class="name">Gengar</h3>
-      <small class="type">Type: <span>Ghost</span></small>
+      <span class="number">#${id}</span>
+      <h3 class="name">${name}</h3>
+      <small class="type">Type: <span>${type}</span></small>
     </div>
-  </div>
   `
   pokemonEl.innerHTML = pokemonInnerHTML
+  pokeContainer.appendChild(pokemonEl)
 }
 
 fetchPokemons()
